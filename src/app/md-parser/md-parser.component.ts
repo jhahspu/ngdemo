679cdecf8509e68baef65c939f2cf3a92adc7e34
jhahspu <jhahspu@outlook.com>
2021-06-21 21:03:29 +0100

    markdown parser

--- src/app/md-parser/md-parser.component.ts
+++ src/app/md-parser/md-parser.component.ts
@@ -13,4 +13,49 @@ export class MdParserComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  markdown = `# Markdown __rulez__!
+---
+
+## Syntax highlight
+
+\`\`\`typescript
+const language = 'typescript';
+\`\`\`
+
+---
+
+\`\`\`docker
+FROM node:15.4
+
+WORKDIR /app
+COPY package*.json .
+RUN npm install
+
+COPY . .
+
+CMD npm run start:dev
+
+\`\`\`
+
+
+---
+
+
+\`\`\`go
+package main
+
+func main() {
+
+}
+\`\`\`
+
+### Lists
+1. Ordered list
+2. Another bullet point
+   - Unordered list
+   - Another unordered bullet
+
+### Blockquote
+> Blockquote to the max`;
+
 }
--- src/app/md-parser/md-parser.module.ts
+++ src/app/md-parser/md-parser.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdParserComponent } from './md-parser.component';
+import { FormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
 
-
+import 'prismjs';
+import 'prismjs/components/prism-typescript.min.js';
+import 'prismjs/components/prism-docker.min.js';
+import 'prismjs/components/prism-go.min.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
 
 @NgModule({
   declarations: [
     MdParserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MarkdownModule.forRoot()
   ]
 })
 export class MdParserModule { }
