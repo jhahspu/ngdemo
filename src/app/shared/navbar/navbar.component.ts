d709a5aeab573729abc3ec065b59ac7e192493d5
jhahspu <jhahspu@outlook.com>
2021-06-22 13:20:12 +0100

    burger menu

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -4,4 +4,13 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ngdemo-navbar',
   templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+
+  toggleMenu(): void {
+    document.querySelector('.menu')?.classList.toggle('open');
+    document.querySelector('.nav')?.classList.toggle('closed');
+  }
+
+
+}
